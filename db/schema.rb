635564b2743dcd690eaf22220e969867d96b9509
satsuma-toshiaki <satsuma-toshiaki@outlook.jp>
2016-11-07 12:32:23 +0900

    育成論修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107015438) do
+ActiveRecord::Schema.define(version: 20161107032541) do
 
   create_table "buildtags", force: :cascade do |t|
     t.string   "name"
